169422655fd94d01201e6e420689a43e85a61997
enjangms <enjangsasana@gmail.com>
2023-11-23 23:12:45 +0700

    first commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Profile Enjang Margha</title>
+  </head>
+  <body>
+    
+  <section class="vh-100" style="background-color: #61677A;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-lg-9 col-lg-7 col-xl-5">
+        <div class="card" style="border-radius: 12px;">
+          <div class="card-body p-4">
+            <div class="d-flex text-black">
+              <div class="flex-shrink-0">
+                <img src="https://i.pinimg.com/564x/c4/4d/02/c44d02e9861bb0d785f12233fc4acd3e.jpg"
+                  alt="Generic placeholder image" class="img-fluid"
+                  style="width: 180px; border-radius: 10px;">
+              </div>
+              <div class="flex-grow-1 ms-3">
+                <h5 class="mb-1">Enjang Margha Sasana</h5>
+                <p class="mb-2 pb-1" style="color: #2b2a2a;">Meskipun pikiranku terganggu yang penting ada rokok di saku ku</p>
+                <div class="d-flex justify-content-start rounded-3 p-2 mb-2"
+                  style="background-color: #efefef;">
+                  <div>
+                    <p class="small text-muted mb-1">NIM</p>
+                    <p class="mb-0">234311013</p>
+                  </div>
+                  <div class="px-3">
+                    <p class="small text-muted mb-1">Kelas</p>
+                    <p class="mb-0">TRPL 1A</p>
+                  </div>
+                </div>
+                <div class="d-flex pt-1">
+                   <button type="button" class="btn btn-outline-secondary me-1 flex-grow-1" onclick="window.open('https://www.instagram.com/enjang_margha/', '_blank')">Follow</button>  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
